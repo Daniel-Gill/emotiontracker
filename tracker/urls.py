75bbc493cd4552e2abf95bbf13a5e8c81c0d7bc7
Daniel Gill <56364196+Daniel-Gill@users.noreply.github.com>
2023-10-08 01:08:29 +0100

    Added login basics

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login", views.login, name="login"),
+    path("login", views.loginview, name="login"),
     path("login/auth", views.loginauth, name="loginauth"),
     path("logout", views.logoutreq, name="logout"),
     path("dashboard", views.dashboard, name="dashboard"),
--- tracker/views.py
+++ tracker/views.py
@@ -1,15 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the tracker index.")
 
+@login_required(login_url="login")
 def dashboard(request):
-    return HttpResponse("Hello, world. You're at the tracker dashboard.")
-
-def login(request):
+    return render(request, "tracker/dashboard.html")
+    
+def loginview(request):
     return render(request, "tracker/login.html")
 
 def loginauth(request):
@@ -17,11 +19,11 @@ def loginauth(request):
     password = request.POST["password"]
     user = authenticate(request, username=username, password=password)
     if user is not None:
-        login(request)
-        
+        login(request, user)
+        return redirect("dashboard")
     else:
         return HttpResponse("Invalid Credentials")
     
 def logoutreq(request):
     logout(request)
-    redirect("index")
+    return redirect("index")
